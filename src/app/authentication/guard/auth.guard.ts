f85979d392ee5046bd826af08418858064135a73
B_Chandu <bchandrashekargoud10@gmail.com>
2025-01-05 09:33:27 +0530

    added auth guard and implemented guard service

---
+++ src/app/authentication/guard/auth.guard.ts
@@ -0,0 +1,9 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthenticationService } from '../services/authentication.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authenticationService = inject(AuthenticationService);
+  const router = inject(Router);
+  return authenticationService.isUserLoggedIn() ?  true : router.navigate(['login']);
+};
--- src/app/authentication/services/authentication.service.ts
+++ src/app/authentication/services/authentication.service.ts
@@ -12,6 +12,14 @@ export class AuthenticationService {
     // this.authenticationRestService.validateCredentials(loginForm).subscribe((response: any) => {
     //   console.log(response);
     // });
-    sessionStorage.setItem('credentials', JSON.stringify(loginForm));
+    if(loginForm.userName === 'chandu'){
+      sessionStorage.setItem('credentials', JSON.stringify(loginForm));
+      return true;
+    }
+    return false;
+  }
+
+  isUserLoggedIn() : boolean {
+    return (sessionStorage.getItem('credentials') !== null)
   }
 }
